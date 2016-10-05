264fed69b18849943ae273ef765dcb32d5b50bd7
Pablo Pfister <p.pfister@ticino.com>
2016-10-05 13:48:50 +0200

    add SimpleRunActivity and button in MainActivity to get here

--- app/src/main/java/ch/epfl/sweng/project/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/MainActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Your app's main activity.
@@ -17,5 +19,10 @@ public final class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toSimpleRunTapped(View view) {
+        Intent intent = new Intent(this, SimpleRunActivity.class);
+        startActivity(intent);
     }
 }
