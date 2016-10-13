363be93ad111e7c8e4ebdba0c3bf1b8e56eab783
Lucio <lucio.romerio@epfl.ch>
2016-10-13 17:08:33 +0200

    fix permission warning, bug disovered with Android 6.0+ SDK 22+

--- app/src/main/java/ch/epfl/sweng/project/Activities/SideBarActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/Activities/SideBarActivity.java
@@ -1,12 +1,17 @@
 package ch.epfl.sweng.project.Activities;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
@@ -18,6 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.android.multidex.ch.epfl.sweng.project.AppRunnest.R;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -36,11 +42,13 @@ import ch.epfl.sweng.project.Fragments.RunningMapFragment;
 public class SideBarActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, LocationDemo.OnFragmentInteractionListener {
 
+    // Constants
+    public static final int PERMISSION_REQUEST_CODE_FINE_LOCATION = 1;
+
+    // Attributes
     private FragmentManager fragmentManager;
     private Fragment mFragment;
-
-
-
+    private boolean locationPermissionGranted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -140,5 +148,35 @@ public class SideBarActivity extends AppCompatActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+
+            case PERMISSION_REQUEST_CODE_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    locationPermissionGranted = true;
+                    Toast.makeText(getApplicationContext(),"You can now start a Run.",Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    locationPermissionGranted = false;
+                    Toast.makeText(getApplicationContext(),"Permission Denied, you cannot start a Run.",Toast.LENGTH_LONG).show();
+                }
+                break;
+
+        }
+
+    }
+
+    public boolean getLocationPermissionGranted() {
+        return locationPermissionGranted;
+    }
+
+    public void setLocationPermissionGranted(boolean granted) {
+        locationPermissionGranted = granted;
     }
 }
--- app/src/main/java/ch/epfl/sweng/project/Fragments/LocationDemo.java
+++ app/src/main/java/ch/epfl/sweng/project/Fragments/LocationDemo.java
@@ -1,21 +1,28 @@
 package ch.epfl.sweng.project.Fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.multidex.ch.epfl.sweng.project.AppRunnest.R;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
@@ -28,6 +35,7 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 
+import ch.epfl.sweng.project.Activities.SideBarActivity;
 import ch.epfl.sweng.project.Model.CheckPoint;
 import ch.epfl.sweng.project.Model.Run;
 
@@ -37,15 +45,14 @@ import ch.epfl.sweng.project.Model.Run;
  * Activities that contain this fragment must implement the
  * {@link LocationDemo.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link LocationDemo#newInstance} factory method to
+ * Use the {@link LocationDemo} factory method to
  * create an instance of this fragment.
  */
 public class LocationDemo extends Fragment implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener,
-        ResultCallback<LocationSettingsResult>
-{
+        ResultCallback<LocationSettingsResult> {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -62,39 +69,26 @@ public class LocationDemo extends Fragment implements
     private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
     private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
 
-    /** My attributes */
-    private GoogleApiClient mGoogleApiClient;
+    /** Layout */
 
     private Button mStartUpdatesButton;
     private Button mStopUpdatesButton;
     private TextView mLatitudeText;
     private TextView mLongitudeText;
+    private TextView mNbCheckPointLabel;
+    private TextView mNbCheckPointValue;
+
+    /** My attributes */
+    private GoogleApiClient mGoogleApiClient;
 
     private LocationRequest mLocationRequest;
     protected LocationSettingsRequest mLocationSettingsRequest;
 
     private boolean mRequestingLocationUpdates;
 
+    private  int checkPointSaved;
     private CheckPoint mLastCheckPoint;
     private Run mRun;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LocationDemo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LocationDemo newInstance(String param1, String param2) {
-        LocationDemo fragment = new LocationDemo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -111,12 +105,16 @@ public class LocationDemo extends Fragment implements
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_location_demo, container, false);
 
+        checkPermission();
 
         mLatitudeText = (TextView) view.findViewById(R.id.latitude_text);
         mLongitudeText = (TextView) view.findViewById(R.id.longitude_text);
 
-        // Initialize mRun
-        mRun = new Run();
+        mNbCheckPointLabel = (TextView) view.findViewById(R.id.nb_checkPoint_label);
+        mNbCheckPointValue = (TextView) view.findViewById(R.id.nb_checkPoint_value);
+        mNbCheckPointLabel.setVisibility(View.INVISIBLE);
+        mNbCheckPointValue.setVisibility(View.INVISIBLE);
+        checkPointSaved = 0;
 
         // Buttons setup
         mStartUpdatesButton = (Button) view.findViewById(R.id.start_updates_button);
@@ -125,8 +123,18 @@ public class LocationDemo extends Fragment implements
             @Override
             public void onClick(View v) {
 
+                mNbCheckPointLabel.setVisibility(View.VISIBLE);
+                mNbCheckPointValue.setVisibility(View.VISIBLE);
+
                 mStartUpdatesButton.setVisibility(View.INVISIBLE);
                 mStopUpdatesButton.setVisibility(View.VISIBLE);
+                mRun = new Run();
+
+                if(mLastCheckPoint != null) {
+                    mRun.start(mLastCheckPoint);
+                    ++checkPointSaved;
+                    updateUI();
+                }
 
                 if (!mRequestingLocationUpdates) {
                     mRequestingLocationUpdates = true;
@@ -149,6 +157,8 @@ public class LocationDemo extends Fragment implements
             }
         });
 
+        setButtonsEnabledState();
+
 
         mRequestingLocationUpdates = false;
 
@@ -161,7 +171,7 @@ public class LocationDemo extends Fragment implements
         return view;
     }
 
-    private synchronized  void createGoogleApiClient() {
+    private synchronized void createGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(getContext())
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -229,7 +239,30 @@ public class LocationDemo extends Fragment implements
         }
     }
 
+
+
+    public void checkPermission(){
+        int fineLocation = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (fineLocation != PackageManager.PERMISSION_GRANTED){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, SideBarActivity.PERMISSION_REQUEST_CODE_FINE_LOCATION);
+            }
+
+        } else {
+            ((SideBarActivity)getActivity()).setLocationPermissionGranted(true);
+        }
+    }
+
     private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            checkPermission();
+            setButtonsEnabledState();
+        }
+
         LocationServices.FusedLocationApi.requestLocationUpdates(
                 mGoogleApiClient,
                 mLocationRequest,
@@ -243,21 +276,28 @@ public class LocationDemo extends Fragment implements
         });
     }
 
+
     private void setButtonsEnabledState() {
         if (mRequestingLocationUpdates) {
             mStartUpdatesButton.setEnabled(false);
             mStopUpdatesButton.setEnabled(true);
         } else {
-            mStartUpdatesButton.setEnabled(true);
             mStopUpdatesButton.setEnabled(false);
+
+            if ( ((SideBarActivity)getActivity()).getLocationPermissionGranted() ) {
+                mStartUpdatesButton.setEnabled(true);
+            } else {
+                mStartUpdatesButton.setEnabled(false);
+            }
         }
     }
 
     private void updateUI() {
         setButtonsEnabledState();
-        if(mLastCheckPoint != null) {
+        if (mLastCheckPoint != null) {
             mLatitudeText.setText(String.valueOf(mLastCheckPoint.getLatitude()));
             mLongitudeText.setText(String.valueOf(mLastCheckPoint.getLongitude()));
+            mNbCheckPointValue.setText(String.valueOf(checkPointSaved));
         }
     }
 
@@ -275,7 +315,6 @@ public class LocationDemo extends Fragment implements
             }
         });
     }
-
 
 
     @Override
@@ -321,14 +360,22 @@ public class LocationDemo extends Fragment implements
 
         // We don't store data yet, we wait that user start the run
         if (mLastCheckPoint == null) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                checkPermission();
+                setButtonsEnabledState();
+            }
             Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
             if(location != null) {
+
                 mLastCheckPoint = new CheckPoint(location);
             }
             updateUI();
         }
 
         if (mRequestingLocationUpdates) {
+
             startLocationUpdates();
         }
     }
@@ -348,8 +395,12 @@ public class LocationDemo extends Fragment implements
         mLastCheckPoint = new CheckPoint(location);
         if(mRun.isRunning()) {
             mRun.update(mLastCheckPoint);
+            ++checkPointSaved;
+
         } else {
             mRun.start(mLastCheckPoint);
+            ++checkPointSaved;
+
         }
         updateUI();
     }
