24151d1954cad5d9e686ec29de661d8c60a9291e
aTobyWanKenobi <tobia.albergoni@epfl.ch>
2016-10-18 21:10:15 +0200

    working firebase demo

--- app/src/main/java/ch/epfl/sweng/project/Fragments/FirebaseFragment.java
+++ app/src/main/java/ch/epfl/sweng/project/Fragments/FirebaseFragment.java
@@ -14,6 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.multidex.ch.epfl.sweng.project.AppRunnest.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import ch.epfl.sweng.project.Model.CheckPoint;
 import ch.epfl.sweng.project.Model.Effort;
@@ -28,20 +31,22 @@ public class FirebaseFragment extends Fragment implements View.OnClickListener {
     private FirebaseFragment.FireBaseFragmentInteractionListener firebaseListener = null;
 
     private FirebaseHelper mFirebaseHelper;
-
     private Effort demoEffort = null;
 
+    // Buttons
     private Button addUser = null;
     private Button addEffort = null;
     private Button retrieveUser = null;
 
+    // Add user
     private EditText idText = null;
     private EditText nameText = null;
 
+    // Add effort
     private EditText idTextRun = null;
 
+    // Retrieve User
     private EditText askId = null;
-
     private TextView retName = null;
     private TextView retRun = null;
 
@@ -89,6 +94,14 @@ public class FirebaseFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    /**
+     * Utility method to rapidly build a <code>CheckPoint</code>
+     *
+     * @param lat   latitude
+     * @param lon   longitude
+     * @param time  time
+     * @return      the desired <code>CheckPoint</code>
+     */
     public CheckPoint buildCheckPoint(double lat, double lon, long time) {
         Location location = new Location("test");
         location.setLatitude(lat);
@@ -97,48 +110,87 @@ public class FirebaseFragment extends Fragment implements View.OnClickListener {
         return new CheckPoint(location);
     }
 
+    /**
+     * Writes the sample <code>Effort</code> in the database, under the specified user
+     *
+     * @param effort    <code>Effort</code> to store
+     */
     private void getAndWriteEffort(Effort effort) {
 
+        // Check EditText args
         if(idTextRun.getText().toString().isEmpty()){
             Toast.makeText(getActivity().getBaseContext(), "Insert something in all fields", Toast.LENGTH_LONG).show();
             return;
         }
 
+        // Get user id from EditText
         String id = idTextRun.getText().toString();
 
-        if (mFirebaseHelper.addEffort(id, demoEffort)) {
-            Toast.makeText(getActivity().getBaseContext(), "Run added", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getActivity().getBaseContext(), "Run not added", Toast.LENGTH_LONG).show();
-        };
+        // Update database
+        mFirebaseHelper.addOrUpdateEffort(id, demoEffort);
+        Toast.makeText(getActivity().getBaseContext(), "Run added", Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Writes a new user to the database or updates existing one
+     */
     private void getAndWriteUser() {
 
+        // Check EditText args
         if(idText.getText().toString().isEmpty() || nameText.getText().toString().isEmpty()) {
             Toast.makeText(getActivity().getBaseContext(), "Insert something in all fields", Toast.LENGTH_LONG).show();
             return;
         }
+
+        // Get user specs from EditText
         String id = idText.getText().toString();
         String name = nameText.getText().toString();
 
-        boolean addSuccessful = mFirebaseHelper.addUser(id, name);
-        if (addSuccessful){
-            Toast.makeText(getActivity().getBaseContext(), "User added", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getActivity().getBaseContext(), "User already exists", Toast.LENGTH_LONG).show();
-        }
+        // Update database
+        mFirebaseHelper.addOrUpdateUser(id, name);
+        Toast.makeText(getActivity().getBaseContext(), "User added", Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Retrieves and displays user information from database
+     */
     private void retrieveUser() {
+
+        // Check EditText args
         if(askId.getText().toString().isEmpty()) {
             Toast.makeText(getActivity().getBaseContext(), "Insert something in all fields", Toast.LENGTH_LONG).show();
             return;
         }
 
+        // Get user id from EditText
         String id = askId.getText().toString();
 
-        retName.setText(mFirebaseHelper.retrieveUserString(id));
+        // Get user information from database and display it in TextView
+        mFirebaseHelper.getDatbase().child("users").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    retName.setText("Name : " + dataSnapshot.child("name").getValue().toString());
+                    if(dataSnapshot.hasChild("efforts")) {
+                        String runs = "Runs : ";
+                        for(DataSnapshot  effort : dataSnapshot.child("efforts").getChildren()) {
+                            runs += effort.getValue().toString();
+                        }
+                        retRun.setText(runs);
+                    } else {
+                        retRun.setText("Efforts : -");
+                    }
+                } else {
+                    retName.setText("Name : this user doesn't exist");
+                    retRun.setText("Efforts : -");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
