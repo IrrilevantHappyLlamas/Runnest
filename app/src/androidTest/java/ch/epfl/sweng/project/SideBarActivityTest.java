05505f2363a406591290ccc97247cc83ffba7185
aTobyWanKenobi <tobia.albergoni@epfl.ch>
2016-10-18 23:42:36 +0200

    add Espresso to gradle, create empty test suite for side bar

---
+++ app/src/androidTest/java/ch/epfl/sweng/project/SideBarActivityTest.java
@@ -0,0 +1,29 @@
+package ch.epfl.sweng.project;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.project.Activities.SideBarActivity;
+
+/**
+ * Test suite for SideBarActivity
+ */
+@RunWith(AndroidJUnit4)
+@LargeTest
+public class SideBarActivityTest {
+
+    @Rule
+    public ActivityTestRule<SideBarActivity> mActivityRule = new ActivityTestRule<SideBarActivity>(SideBarActivity.class);
+
+    @Test
+    public void onProfileTabPressedChangeFragment() {
+
+    }
+
+}
