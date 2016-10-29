1698abaac847c4376b214bb0ea8a38f9606d11bb
Lucio <lucio.romerio@epfl.ch>
2016-10-29 12:33:02 +0200

    Revert "Add signIn click to LoginTest"
    This reverts commit 3091eed2dd8816ccb4fd494350cdededf4dec0c2.

--- app/src/androidTest/java/ch/epfl/sweng/project/LoginTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/LoginTest.java
@@ -15,7 +15,6 @@ import org.junit.runners.MethodSorters;
 import ch.epfl.sweng.project.Activities.LoginActivity;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -34,10 +33,8 @@ public class LoginTest {
 
    @Test
     public void loginAndLogout() {
-       onView(withId(R.id.sign_in_button))
-               .check(matches(isDisplayed()));
-       onView(withId(R.id.sign_in_button))
-               .perform(click());
+        onView(withId(R.id.sign_in_button))
+                .check(matches(isDisplayed()));
     }
 
     /*@Test
