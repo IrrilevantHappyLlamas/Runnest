de7a600a43cbd785ee328285c5226bfc83b5aa43
aTobyWanKenobi <tobia.albergoni@epfl.ch>
2016-11-24 22:59:49 +0100

    test proxy

---
+++ app/src/main/java/ch/epfl/sweng/project/TestProxy.java
@@ -0,0 +1,63 @@
+package ch.epfl.sweng.project;
+
+import java.util.logging.Handler;
+
+import ch.epfl.sweng.project.Model.ChallengeProxy;
+import ch.epfl.sweng.project.Model.CheckPoint;
+
+/**
+ * Challenge proxy for test sessions.
+ */
+public class TestProxy implements ChallengeProxy {
+
+    private Handler handler = null;
+    private android.os.Handler runnableHandler = new android.os.Handler();
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            sendNextPoint();
+            runnableHandler.postDelayed(runnableCode, 1000);
+        }
+    };
+
+    private double lat;
+    private double lon;
+
+    public TestProxy(Handler handler) {
+        this.handler = handler;
+        lat = 45.0;
+        lon = 45.0;
+    }
+
+    private void sendNextPoint() {
+        handler.OnNewDataHandler(new CheckPoint(lat, lon));
+        lat += 0.05;
+        lon += 0.05;
+    }
+
+    @Override
+    public void putData(CheckPoint checkPoint) {
+
+    }
+
+    @Override
+    public void startChallenge() {
+
+    }
+
+    @Override
+    public void deleteChallenge() {
+
+    }
+
+    @Override
+    public void imReady() {
+        handler.isReadyHandler();
+        runnableHandler.post(runnableCode);
+    }
+
+    @Override
+    public void imFinished() {
+        handler.isFinished();
+    }
+}
