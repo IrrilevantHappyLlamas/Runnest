75ea0bed2d25da0e9dac5ac81945df9b9ff04faa
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2016-12-06 19:11:11 +0100

    modified history design

--- app/src/main/java/ch/epfl/sweng/project/Fragments/RunHistoryFragment.java
+++ app/src/main/java/ch/epfl/sweng/project/Fragments/RunHistoryFragment.java
@@ -1,11 +1,13 @@
 package ch.epfl.sweng.project.Fragments;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ListFragment;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
 import com.example.android.multidex.ch.epfl.sweng.project.AppRunnest.R;
 import java.util.List;
 
@@ -30,14 +35,14 @@ import ch.epfl.sweng.project.Model.Run;
  * {@link RunHistoryFragment.onRunHistoryInteractionListener} interface
  * to handle interaction events.
  */
-public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class RunHistoryFragment extends Fragment {
 
     private onRunHistoryInteractionListener mListener;
     private List<Run> runs;
     private List<Challenge> challenges;
     private ArrayAdapter<String> runsAdapter;
     private ArrayAdapter<String> challengesAdapter;
-
+    private TabLayout tabLayout;
     private ListView listView;
 
     private enum RunType {
@@ -51,15 +56,47 @@ public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSe
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_run_history_fragment, container, false);
 
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
-                R.array.spinner_items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
         listView = (ListView) view.findViewById(R.id.list);
+
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setTabTextColors(ContextCompat.getColor(getContext(), R.color.wonColor), ContextCompat.getColor(getContext(), R.color.wonColor));
+
+
+        TabLayout.Tab runTab = tabLayout.newTab();
+        runTab.setText("Run History");
+        tabLayout.addTab(runTab, true);
+
+        TabLayout.Tab challengeTab = tabLayout.newTab();
+        challengeTab.setText("Challenge History");
+        tabLayout.addTab(challengeTab);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                switch(tab.getPosition()) {
+
+                    case 0:
+                        switchDisplayedListView(RunType.SINGLE_RUN);
+                        break;
+                    case 1:
+                        switchDisplayedListView(RunType.CHALLENGE);
+                        break;
+                    default:
+                        throw new IllegalStateException("Illegal RunType value");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         DBHelper dbHelper = new DBHelper(this.getContext());
 
@@ -68,6 +105,8 @@ public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSe
 
         challenges = dbHelper.fetchAllChallenges();
         challengesAdapter = createAdapter(RunType.CHALLENGE);
+
+        switchDisplayedListView(RunType.SINGLE_RUN);
 
         return view;
     }
@@ -87,21 +126,6 @@ public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSe
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-
-        switch(((String) parent.getItemAtPosition(pos))) {
-            case "Single Runs":
-                switchDisplayedListView(RunType.SINGLE_RUN);
-                break;
-            case "Challenges":
-                switchDisplayedListView(RunType.CHALLENGE);
-                break;
-            default:
-                throw new IllegalStateException("Illegal spinner state.");
-        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -165,7 +189,7 @@ public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSe
                     toBeAdapted = new String[challenges.size()];
 
                     for (int i = 0; i < challenges.size(); ++i) {
-                        toBeAdapted[i] = String.valueOf(challenges.get(i).getId());
+                        toBeAdapted[i] = "vs " + challenges.get(i).getOpponentName() + "  " + determineWOrLString(challenges.get(i));
                     }
                 }
                 break;
@@ -174,6 +198,19 @@ public class RunHistoryFragment extends Fragment implements AdapterView.OnItemSe
         }
 
         return new ArrayAdapter<String>(this.getContext(), R.layout.simple_textview, toBeAdapted);
+    }
+
+    private String determineWOrLString(Challenge challenge){
+
+        String result;
+        if(challenge.isWon()){
+            result = "WON";
+        }
+        else{
+            result = "LOST";
+        }
+
+        return result;
     }
     public interface onRunHistoryInteractionListener {
         void onRunHistoryInteraction(Run run);
