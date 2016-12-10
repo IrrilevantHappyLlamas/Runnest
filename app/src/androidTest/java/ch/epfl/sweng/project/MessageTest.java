35e207c709c05d3dd9c69205e89c7ed52e5fa29f
Pablo Pfister <p.pfister@ticino.com>
2016-12-10 12:18:14 +0100

    Message: create new test to cover the whole code

--- app/src/androidTest/java/ch/epfl/sweng/project/MessageTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/MessageTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import ch.epfl.sweng.project.Model.Challenge;
 import ch.epfl.sweng.project.Model.Message;
 
 public class MessageTest {
@@ -79,6 +80,24 @@ public class MessageTest {
         Message testMessage = new Message("me", "me", "me", "me", Message.Type.TEXT, "Hello, world!");
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorChallengeParamsThrowsExceptionWithNullChallengeType() {
+        Date time = new Date();
+        Message testMessage = new Message("me", "you", "me", "you", Message.Type.TEXT, "msg", time, 0, 1, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorScheduleMemoThrowsExceptionWithNullChallengeType() {
+        Date time = new Date();
+        Message testMessage = new Message("me", "you", "me", "you", Message.Type.TEXT, "msg", time, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorScheduleMemoThrowsExceptionWithIncoherentMessageType() {
+        Date time = new Date();
+        Message testMessage = new Message("me", "you", "me", "you", Message.Type.TEXT, "msg", time, Challenge.Type.DISTANCE);
+    }
+
     @Test
     public void gettersReturnSettedValue() {
         String from = "me";
@@ -86,17 +105,25 @@ public class MessageTest {
         String sender = "me";
         String addressee = "you";
         Message.Type type = Message.Type.TEXT;
-        String message = "Hello, world!";
+        String msgTxt = "Hello, world!";
         Date time = new Date();
-        Message testMessage = new Message(from, to, sender, addressee, type, message, time);
+        int firstValue = 0;
+        int secondValue = 1;
+        Challenge.Type challengeType = Challenge.Type.TIME;
+        Message message = new Message(from, to, sender, addressee, type, msgTxt, time, firstValue, secondValue, challengeType);
 
-        Assert.assertTrue(from.equals(testMessage.getFrom()));
-        Assert.assertTrue(to.equals(testMessage.getTo()));
-        Assert.assertTrue(type.equals(testMessage.getType()));
-        Assert.assertTrue(message.equals(testMessage.getMessage()));
-        Assert.assertTrue(time.equals(testMessage.getTime()));
+        Assert.assertTrue(from.equals(message.getFrom()));
+        Assert.assertTrue(to.equals(message.getTo()));
+        Assert.assertTrue(sender.equals(message.getSender()));
+        Assert.assertTrue(addressee.equals(message.getAddressee()));
+        Assert.assertTrue(type.equals(message.getType()));
+        Assert.assertTrue(msgTxt.equals(message.getMessage()));
+        Assert.assertTrue(time.equals(message.getTime()));
+        Assert.assertEquals(firstValue, message.getFirstValue());
+        Assert.assertEquals(secondValue, message.getSecondValue());
+        Assert.assertEquals(challengeType, message.getChallengeType());
 
         String expectedId = from.hashCode() + "_" + time.hashCode();
-        Assert.assertTrue(expectedId.equals(testMessage.getUid()));
+        Assert.assertTrue(expectedId.equals(message.getUid()));
     }
 }
