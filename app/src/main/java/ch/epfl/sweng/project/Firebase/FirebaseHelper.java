5ece22d6ad0906ac2eb311902817d3113753975b
aTobyWanKenobi <tobia.albergoni@epfl.ch>
2016-12-15 21:10:09 +0100

    refactored loginActivity

--- app/src/main/java/ch/epfl/sweng/project/Firebase/FirebaseHelper.java
+++ app/src/main/java/ch/epfl/sweng/project/Firebase/FirebaseHelper.java
@@ -1,10 +1,12 @@
 package ch.epfl.sweng.project.Firebase;
 
+import com.google.android.gms.gcm.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Date;
